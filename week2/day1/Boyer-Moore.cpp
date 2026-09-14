61e7960b0021f84430dfa3f2189c9bb8b65a5987
zpc749 <3562298007@qq.com>
2026-09-14 19:44:58 +0800

    Update Boyer-Moore.cpp

--- week2/day1/Boyer-Moore.cpp
+++ week2/day1/Boyer-Moore.cpp
@@ -1,1 +1,18 @@
+// 力扣169——多数元素
+// 本题采用摩尔投票法Boyer-Moore投票法
 
+int majorityElement(vector<int>& nums)
+{
+  int candidate = -1;
+  int count = 0;
+  for (int num : nums)
+  {
+    if (num == candidata) ++count;
+    else if (--count < 0)
+    {
+      candidate = num;
+      count = 1;
+    }
+  }
+  return candidate;
+}
