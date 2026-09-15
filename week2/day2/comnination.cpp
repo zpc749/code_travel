297838b9e25748ef65ed6bff85e528256cb75f76
zpc749 <3562298007@qq.com>
2026-09-15 19:53:35 +0800

    Update comnination.cpp

--- week2/day2/comnination.cpp
+++ week2/day2/comnination.cpp
@@ -1,1 +1,173 @@
+// 1.组合（77）
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(int n, int k, int startIndex) {
+        if (path.size() == k) {
+            result.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i <= n - (k - path.size()) + 1; i++) { // 优化的地方       // i + (k-path.size()) - 1 <= n
+            path.push_back(i); // 处理节点
+            backtracking(n, k, i + 1);
+            path.pop_back(); // 回溯，撤销处理的节点
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        backtracking(n, k, 1);
+        return result;
+    }
+};
 
+
+
+// 2.组合总和3（216）
+class Solution {
+private:
+    vector<vector<int>> result; // 存放结果集
+    vector<int> path; // 符合条件的结果
+    void backtracking(int targetSum, int k, int sum, int startIndex) {
+        if (sum > targetSum) { // 剪枝操作
+            return; 
+        }
+        if (path.size() == k) {
+            if (sum == targetSum) result.push_back(path);
+            return; // 如果path.size() == k 但sum != targetSum 直接返回
+        }
+        for (int i = startIndex; i <= 9 - (k - path.size()) + 1; i++) { // 剪枝
+            sum += i; // 处理
+            path.push_back(i); // 处理
+            backtracking(targetSum, k, sum, i + 1); // 注意i+1调整startIndex
+            sum -= i; // 回溯
+            path.pop_back(); // 回溯
+        }
+    }
+
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        result.clear(); // 可以不加
+        path.clear();   // 可以不加
+        backtracking(n, k, 0, 1);
+        return result;
+    }
+};
+
+
+
+// 3. 电话号码的字母组合（17）
+class Solution {
+private:
+    const string letterMap[10] = {
+        "", // 0
+        "", // 1
+        "abc", // 2
+        "def", // 3
+        "ghi", // 4
+        "jkl", // 5
+        "mno", // 6
+        "pqrs", // 7
+        "tuv", // 8
+        "wxyz", // 9
+    };
+public:
+    vector<string> result;
+    string s;
+    void backtracking(const string& digits, int index) {
+        if (index == digits.size()) {
+            result.push_back(s);
+            return;
+        }
+        int digit = digits[index] - '0';        // 将index指向的数字转为int
+        string letters = letterMap[digit];      // 取数字对应的字符集
+        for (int i = 0; i < letters.size(); i++) {
+            s.push_back(letters[i]);            // 处理
+            backtracking(digits, index + 1);    // 递归，注意index+1，一下层要处理下一个数字了
+            s.pop_back();                       // 回溯
+        }
+    }
+    vector<string> letterCombinations(string digits) {
+        s.clear();
+        result.clear();
+        if (digits.size() == 0) {
+            return result;
+        }
+        backtracking(digits, 0);
+        return result;
+    }
+};
+
+
+
+// 4. 组合总和（39）
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(vector<int>& candidates, int target, int sum, int startIndex) {
+        if (sum == target) {
+            result.push_back(path);
+            return;
+        }
+
+        // 如果 sum + candidates[i] > target 就终止遍历
+        for (int i = startIndex; i < candidates.size() && sum + candidates[i] <= target; i++) {
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            backtracking(candidates, target, sum, i);
+            sum -= candidates[i];
+            path.pop_back();
+
+        }
+    }
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        result.clear();
+        path.clear();
+        sort(candidates.begin(), candidates.end()); // 需要排序
+        backtracking(candidates, target, 0, 0);
+        return result;
+    }
+};
+
+
+
+//5. 组合总和2（40）
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(vector<int>& candidates, int target, int sum, int startIndex, vector<bool>& used) {
+        if (sum == target) {
+            result.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i < candidates.size() && sum + candidates[i] <= target; i++) {
+            // used[i - 1] == true，说明同一树枝candidates[i - 1]使用过
+            // used[i - 1] == false，说明同一树层candidates[i - 1]使用过
+            // 要对同一树层使用过的元素进行跳过
+            if (i > 0 && candidates[i] == candidates[i - 1] && used[i - 1] == false) {
+                continue;
+            }
+            sum += candidates[i];
+            path.push_back(candidates[i]);
+            used[i] = true;
+            backtracking(candidates, target, sum, i + 1, used); // 和39.组合总和的区别1，这里是i+1，每个数字在每个组合中只能使用一次
+            used[i] = false;
+            sum -= candidates[i];
+            path.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<bool> used(candidates.size(), false);
+        path.clear();
+        result.clear();
+        // 首先把给candidates排序，让其相同的元素都挨在一起。
+        sort(candidates.begin(), candidates.end());
+        backtracking(candidates, target, 0, 0, used);
+        return result;
+    }
+};
