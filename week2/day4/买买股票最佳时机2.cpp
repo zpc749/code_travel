418633ccb1b98b9e8b7c25288911f815e6cddc85
zpc749 <3562298007@qq.com>
2026-09-17 16:31:57 +0800

    Create 买买股票最佳时机2.cpp

---
+++ week2/day4/买买股票最佳时机2.cpp
@@ -0,0 +1,12 @@
+// 买买股票最佳时机2（122）
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int result = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            result += max(prices[i] - prices[i - 1], 0);
+        }
+        return result;
+    }
+};
