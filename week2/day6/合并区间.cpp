bec30e8b2065daaca00c616fe89ad603278bb85d
zpc749 <3562298007@qq.com>
2026-09-19 23:02:01 +0800

    Create 合并区间.cpp

---
+++ week2/day6/合并区间.cpp
@@ -0,0 +1,24 @@
+// 合并区间（56）
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> result;
+        if (intervals.size() == 0) return result; // 区间集合为空直接返回
+        // 排序的参数使用了lambda表达式
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b){return a[0] < b[0];});
+
+        // 第一个区间就可以放进结果集里，后面如果重叠，在result上直接合并
+        result.push_back(intervals[0]); 
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (result.back()[1] >= intervals[i][0]) { // 发现重叠区间
+                // 合并区间，只更新右边界就好，因为result.back()的左边界一定是最小值，因为我们按照左边界排序的
+                result.back()[1] = max(result.back()[1], intervals[i][1]); 
+            } else {
+                result.push_back(intervals[i]); // 区间不重叠 
+            }
+        }
+        return result;
+    }
+};
