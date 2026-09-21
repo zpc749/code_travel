6a55e5c7b7875027d49c407bb43f306118f81209
zpc749 <3562298007@qq.com>
2026-09-21 19:13:48 +0800

    Create 接雨水.cpp

---
+++ week3/day1/接雨水.cpp
@@ -0,0 +1,24 @@
+// 接雨水（42）
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int sum = 0;
+        for (int i = 0; i < height.size(); i++) {
+            // 第一个柱子和最后一个柱子不接雨水
+            if (i == 0 || i == height.size() - 1) continue;
+
+            int rHeight = height[i]; // 记录右边柱子的最高高度
+            int lHeight = height[i]; // 记录左边柱子的最高高度
+            for (int r = i + 1; r < height.size(); r++) {
+                if (height[r] > rHeight) rHeight = height[r];
+            }
+            for (int l = i - 1; l >= 0; l--) {
+                if (height[l] > lHeight) lHeight = height[l];
+            }
+            int h = min(lHeight, rHeight) - height[i];
+            if (h > 0) sum += h;
+        }
+        return sum;
+    }
+};
