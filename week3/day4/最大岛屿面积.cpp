db7501a71848d5fc4150f6a9f8c742a070c65fd6
zpc749 <3562298007@qq.com>
2026-09-24 16:44:18 +0800

    Create 最大岛屿面积.cpp

---
+++ week3/day4/最大岛屿面积.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int count;
+int dir[4][2] = {0, 1, 1, 0, -1, 0, 0, -1}; // 四个方向
+void dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, int x, int y) {
+    for (int i = 0; i < 4; i++) {
+        int nextx = x + dir[i][0];
+        int nexty = y + dir[i][1];
+        if (nextx < 0 || nextx >= grid.size() || nexty < 0 || nexty >= grid[0].size()) continue;  // 越界了，直接跳过
+        if (!visited[nextx][nexty] && grid[nextx][nexty] == 1) { // 没有访问过的 同时 是陆地的
+            visited[nextx][nexty] = true;
+            count++;
+            dfs(grid, visited, nextx, nexty);
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> grid(n, vector<int>(m, 0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    vector<vector<bool>> visited(n, vector<bool>(m, false));
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (!visited[i][j] && grid[i][j] == 1) {
+                count = 1;  // 因为dfs处理下一个节点，所以这里遇到陆地了就先计数，dfs处理接下来的相邻陆地
+                visited[i][j] = true;
+                dfs(grid, visited, i, j); // 将与其链接的陆地都标记上 true
+                result = max(result, count);
+            }
+        }
+    }
+    cout << result << endl;
+
+}
