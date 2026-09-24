b64da2ab433d11ea8717bc010bbea9f70887ca7e
zpc749 <3562298007@qq.com>
2026-09-24 16:46:12 +0800

    Create 沉没孤岛.cpp

---
+++ week3/day4/沉没孤岛.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int dir[4][2] = {-1, 0, 0, -1, 1, 0, 0, 1}; // 保存四个方向
+void dfs(vector<vector<int>>& grid, int x, int y) {
+    grid[x][y] = 2;
+    for (int i = 0; i < 4; i++) { // 向四个方向遍历
+        int nextx = x + dir[i][0];
+        int nexty = y + dir[i][1];
+        // 超过边界
+        if (nextx < 0 || nextx >= grid.size() || nexty < 0 || nexty >= grid[0].size()) continue;
+        // 不符合条件，不继续遍历
+        if (grid[nextx][nexty] == 0 || grid[nextx][nexty] == 2) continue;
+        dfs (grid, nextx, nexty);
+    }
+    return;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> grid(n, vector<int>(m, 0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    // 步骤一：
+    // 从左侧边，和右侧边 向中间遍历
+    for (int i = 0; i < n; i++) {
+        if (grid[i][0] == 1) dfs(grid, i, 0);
+        if (grid[i][m - 1] == 1) dfs(grid, i, m - 1);
+    }
+
+    // 从上边和下边 向中间遍历
+    for (int j = 0; j < m; j++) {
+        if (grid[0][j] == 1) dfs(grid, 0, j);
+        if (grid[n - 1][j] == 1) dfs(grid, n - 1, j);
+    }
+    // 步骤二、步骤三
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (grid[i][j] == 1) grid[i][j] = 0;
+            if (grid[i][j] == 2) grid[i][j] = 1;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
